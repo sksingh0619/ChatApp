246df27dbeef1714ccb1d891875d42e3b022990c
SudhanshuAESS <89290007+sksingh0619@users.noreply.github.com>
2024-05-31 15:08:16 +0530

    Deployed Url

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ require("dotenv").config();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
   },
 });
 
